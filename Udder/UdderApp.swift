719cc7962c406ef822ab72ad2c608d18a2063bb0
Ladislav Kroupa <ladislav.kroupa99@gmail.com>
2023-02-08 13:31:35 +0100

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Ladislav Kroupa on 08.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
